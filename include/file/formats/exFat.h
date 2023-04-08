5c7b1f0546279215bd53ef4c7d23d5b0dba1d0ab
Alex <17982529+alexm622@users.noreply.github.com>
2023-04-08 14:46:14 +0000

    exfat ebs, oem, and main reserved done

--- include/file/formats/exFat.h
+++ include/file/formats/exFat.h
@@ -3,6 +3,11 @@
 
 #include "file/formats/format.h"
 
+#include <chrono>
+
+#include <math.h>
+#include <time.h>
+
 class ExFat : public Format{
   public:
   ExFat(char *fname, lli size) 
@@ -19,6 +24,43 @@ private:
   char *fname;
   lli size;
   Format_e f;
+
+  //variables necessary for code
+
+  //BS
+  std::string str = "cramped ";
+  std::string fsName = "ExFat  ";
+
+  const int BYTES_PER_SECTOR_SHIFT = 9;
+
+  //these should all be dynamic at some point
+  const int BYTES_PER_SECTOR = 512;
+  const lli NUM_SECTORS = size / BYTES_PER_SECTOR / 4;
+  const int CLUSTER_COUNT = 4;
+  const int FAT_OFFSET = 24;
+  const int FAT_LENGTH = ((CLUSTER_COUNT + 2) * pow(2,2))/(pow(2, BYTES_PER_SECTOR_SHIFT));
+  const int CLUSTER_HEAP_OFFSET = FAT_OFFSET+FAT_LENGTH*CLUSTER_COUNT;
+  const int FIRST_CLUSTER_OF_ROOT_DIRECTORY = 2;
+
+  
+  //EBS
+  const int EBS_START = 0xfe + 0x02;
+  const int EBS_SIZE = pow(2,BYTES_PER_SECTOR_SHIFT) - 4;
+
+  ///OEMP
+  const int OEMP_START = EBS_START + EBS_SIZE + 0x04;
+  const uint64_t EPOCH = std::chrono::system_clock::now().time_since_epoch().count();
+  const uint64_t GUID_p1 = (uint64_t) EPOCH - 272431 * 37 / 8 - (EPOCH % 2843);
+  const uint64_t GUID_p2 = GUID_p1 + (uint64_t) pow(215234, 2) / 3 - (uint64_t) sqrt(EPOCH);
+  const int OEMP_SECTION_SIZE = 48;
+  
+  const uint64_t FLASH_p1 = 0x0A0C7E4633994021;
+  const uint64_t FLASH_p2 = 0x90C8FA6D389C4BA2;
+
+  //FAT_RS
+  const int FAT_RS_START = 0x1e0 + pow(2,BYTES_PER_SECTOR_SHIFT) - 480;
+
+  
   
 };
 
--- src/file/formats/exFat.cpp
+++ src/file/formats/exFat.cpp
@@ -4,8 +4,9 @@
 
 #include <fstream>
 #include <ios>
-#include <math.h>
-#include <time.h>
+
+#include <inttypes.h>
+
 
 void ExFat::format(){
   writeBS();
@@ -16,18 +17,14 @@ void ExFat::format(){
 }
 
 void ExFat::writeBS(){
-  printf("writing BS");
-  const int BYTES_PER_SECTOR_SHIFT = 9;
-  //initialization
+  printf("writing BS\n");  //initialization
   std::fstream file;
   file.open(fname);
   file.seekp(0);
   char *data = new char[3];
   unsigned char *buf = new unsigned char[4];
 
-  std::string str = "cramped ";
-  std::string fsName = "ExFat  ";
-
+  
   //jumpboot data
   data[0]=(char)0xEB;
   data[1]=(char)0x76;
@@ -37,7 +34,7 @@ void ExFat::writeBS(){
   file.write(data,3);
 
   //filesystemname
-  file.write(fsName.c_str(), 8);
+  file.write(this->fsName.c_str(), 8);
 
   //must be zero
   buf[0] = 0x00;
@@ -49,40 +46,30 @@ void ExFat::writeBS(){
 
   //volume length
 
-  //this should be dynamic at some point
-  const int bytes_per_sector = 512;
-  //calculate the number
-  const lli num_sectors = size / bytes_per_sector / 4;
-  const int cluster_count = 4;
-
 
   
-  Converter::IntToLittleEndianHex(buf, num_sectors);
+  Converter::IntToLittleEndianHex(buf, this->NUM_SECTORS);
   file.write(reinterpret_cast<const char *>(buf), 4);
 
   //fat offset field
-  const int FAT_OFFSET = 24;
-  Converter::IntToLittleEndianHex(buf, FAT_OFFSET);
+    Converter::IntToLittleEndianHex(buf, this->FAT_OFFSET);
   file.write(reinterpret_cast<const char *>(buf), 4);
 
   //FATLength
   //this will eventually be dynamic as well
-  int fatLength = ((cluster_count + 2) * pow(2,2))/(pow(2, BYTES_PER_SECTOR_SHIFT));
-  Converter::IntToLittleEndianHex(buf, fatLength);
+    Converter::IntToLittleEndianHex(buf, this->FAT_LENGTH);
   file.write(reinterpret_cast<const char *>(buf), 4);
 
   //cluster heap offset
-  const int CLUSTER_HEAP_OFFSET = FAT_OFFSET+fatLength*cluster_count;
-  Converter::IntToLittleEndianHex(buf, CLUSTER_HEAP_OFFSET);
+    Converter::IntToLittleEndianHex(buf, this->CLUSTER_HEAP_OFFSET);
   file.write(reinterpret_cast<const char *>(buf), 4);
 
   //cluster count
-  Converter::IntToLittleEndianHex(buf, cluster_count);
+  Converter::IntToLittleEndianHex(buf, this->CLUSTER_COUNT);
   file.write(reinterpret_cast<const char *>(buf), 4);
 
   //first cluster of root directory
-  const int FIRST_CLUSTER_OF_ROOT_DIRECTORY = 2;
-  Converter::IntToLittleEndianHex(buf, FIRST_CLUSTER_OF_ROOT_DIRECTORY);
+    Converter::IntToLittleEndianHex(buf, this->FIRST_CLUSTER_OF_ROOT_DIRECTORY);
   file.write(reinterpret_cast<const char *>(buf),4);
 
   //serial number
@@ -126,10 +113,54 @@ void ExFat::writeBS(){
   buf[0] = 0x55;
   buf[1] = 0xAA;
   file.write(reinterpret_cast<const char*>(buf),2);
-  printf("done writing bootsector");
+  printf("done writing bootsector\n");
+  delete[] buf;
 }
 
-void ExFat::writeEBS() {};
-void ExFat::writeOEMP(){};
+void ExFat::writeEBS() {
+  printf("writings extended boot sector\n");
+  std::fstream file;
+  file.open(fname);
+  file.seekp(EBS_START);
+
+  file.seekg(EBS_SIZE, std::ios::cur);
+  
+  unsigned char *buf = new unsigned char[4];
+  buf[0] = 0x00;
+  buf[1] = 0x00;
+  buf[2] = 0x55;
+  buf[3] = 0xAA;
+  file.write(reinterpret_cast<const char *>(buf), 4);
+
+  printf("done writing extended boot sector\n");
+  delete[] buf;
+ 
+
+};
+void ExFat::writeOEMP(){
+  std::fstream file;
+  file.open(fname);
+  file.seekp(OEMP_START);
+  //guid 1
+  unsigned char *buf = new unsigned char[4];
+  
+  //print guid
+  printf("GUID: %#" PRIx64 "%" PRIx64 "\n",GUID_p1,GUID_p2);
+
+  //part one
+  Converter::IntToLittleEndianHex(buf, GUID_p1>>32);
+  file.write(reinterpret_cast<const char*>(buf),4);
+  Converter::IntToLittleEndianHex(buf, GUID_p1);
+  file.write(reinterpret_cast<const char*>(buf),4);
+
+  //part 2
+  Converter::IntToLittleEndianHex(buf, GUID_p2>>32);
+  file.write(reinterpret_cast<const char*>(buf),4);
+  Converter::IntToLittleEndianHex(buf, GUID_p2);
+  file.write(reinterpret_cast<const char*>(buf),4);
+
+  file.seekg(32);
+
+};
 void ExFat::writeRS(){};
 void ExFat::writeBC(){};
